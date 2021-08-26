af5f0048f8250aebb89a4c13dcd61482cde14800
Kawan Manzini <kawanmanzini@gmail.com>
2021-08-26 12:37:54 -0300

    commit 5

---
+++ VAVRIAVE_CONSTANTE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VAVRIAVE_CONSTANTE
+{
+    class Program
+    {
+
+        //capslock atibado e _ para separar o nome de qualquer constante
+        // não usar static pois toda constante ja é static
+
+        public const int SAIDA_PROGRAMA = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("digite " + SAIDA_PROGRAMA + " para sair do programa");
+                int valor = int.Parse(Console.ReadLine());
+
+                if (SAIDA_PROGRAMA == valor)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
